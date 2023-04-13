e4b2939c3de458ee2d2f9b4b53d29ff0b0233417
LiSazonova <lilissazonova@gmail.com>
2023-04-13 18:01:36 +0300

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,28 @@
 const startBtn = document.querySelector('[data-start]');
-console.log(startBtn);
+const stopBtn = document.querySelector('[data-stop]');
+let intervalId = null;
+
+startBtn.addEventListener('click', onClickStart);
+stopBtn.addEventListener('click', onClickStop);
+
+function onClickStart() {
+    startBtn.setAttribute('disabled', true);
+    
+    intervalId = setInterval(changeColorBody, 1000);
+    
+    stopBtn.removeAttribute('disabled');
+}
+
+function onClickStop() {
+    startBtn.removeAttribute('disabled');
+    stopBtn.setAttribute('disabled', true);
+    clearInterval(intervalId);
+}
+
+function changeColorBody() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
